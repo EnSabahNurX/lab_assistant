48f5524cc0128cd7795733b20bca954212925475
Ricardo Almeida <ricklimadealmeida@gmail.com>
2025-05-01 03:36:10 +0100

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,19 +1,3 @@
 @echo off
-REM 1. Definir o diretório de instalação do Python
-set "TARGET_DIR=%~dp0Python"
-set "PYTHON_EXE=%TARGET_DIR%\python.exe"
-
-REM 2. Verificar se o Python está instalado
-if not exist "%PYTHON_EXE%" (
-    echo ❌ Python não foi encontrado em "%TARGET_DIR%".
-    pause
-    exit /b
-)
-
-REM 3. Executar o script main.py com o Python instalado localmente
-echo [INFO] Executando o script main.py...
-"%PYTHON_EXE%" "%~dp0main.py"
-
-echo.
-echo ✅ Execução concluída.
-pause
+start "" "%~dp0Python\pythonw.exe" "%~dp0CPK.py"
+exit
